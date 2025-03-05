2fa43b2e82c1d6d11632a1000cc3af9908761a5b
Juraj Stefanic <jurajstefanic@outlook.com>
2025-03-05 15:21:18 +0100

    refactor: change variable declaration from let to const

--- app/page.tsx
+++ app/page.tsx
@@ -279,7 +279,7 @@ export default function Home() {
       return;
     }
 
-    let newBestTimes = [];
+    const newBestTimes = [];
     const uniqueAudienceTimezones = Array.from(new Set(audienceTimezones));
 
     for (let hour = 7; hour <= 21; hour++) {
