ae2dce7fd6250e16a10c15a398f7cc57c811a4be
Juraj Stefanic <jurajstefanic@outlook.com>
2025-03-06 15:01:47 +0100

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/assets/icons/favicon.svg" />
       </head>
       <body>
         {children}
